42ac69ccff8df7cdeebedb1b208308819377e532
Dheeraj Panneer Selvam <dpanneer@usc.edu>
2025-12-27 23:31:11 +0530

    RAG and Agent dev

---
+++ agents/app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import os
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_ollama import OllamaEmbeddings, OllamaLLM
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+import time
+
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+if "vector" not in st.session_state:
+    st.session_state.embeddings=OllamaEmbeddings(model="mxbai-embed-large")
+    st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
+    st.session_state.docs=st.session_state.loader.load()
+
+    st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20])
+    st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+
+st.title("Llama3.2 Demo")
+llm= OllamaLLM(model="llama3.2")
+
+prompt=ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Questions:{input}
+
+    """
+    )
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+input_text=st.text_input("Input you prompt here")
+
+if input_text:
+    start=time.process_time()
+    response=retrieval_chain.invoke({"input":input_text})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
