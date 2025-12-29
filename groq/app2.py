b2bae811f31dc3024f4bc56bfe37fdb4e3173521
Dheeraj Panneer Selvam <dpanneer@usc.edu>
2025-12-29 13:29:58 +0530

    Datastax AstraDB retrival developed

---
+++ groq/app2.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import os
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_ollama import OllamaEmbeddings, OllamaLLM
+from langchain_groq import ChatGroq
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+import time
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGSMITH_PROJECT"] = "Groq_Test"
+
+api_key_groq = os.getenv("GROQ_API_KEY")
+
+def create_vector_store():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=OllamaEmbeddings(model="mxbai-embed-large")
+        st.session_state.loader=PyPDFDirectoryLoader("./us_census")
+        st.session_state.docs=st.session_state.loader.load()
+
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:10])
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+
+st.title("Groq Demo")
+
+if st.button("Generate Document Embeddings"):
+    create_vector_store()
+    st.write("Vector Store DB created")
+
+llm = ChatGroq(groq_api_key=api_key_groq, model_name="llama-3.1-8b-instant")
+
+prompt=ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Questions:{input}
+
+    """
+    )
+
+input_text=st.text_input("Input you prompt here")
+
+if input_text:
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    start=time.process_time()
+    response=retrieval_chain.invoke({"input":input_text})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
